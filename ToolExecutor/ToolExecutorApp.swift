706e5771ea19b98d084a77f81770500adc428a99
YU-ERH <yuerhpan@uwm.edu>
2025-07-18 15:42:56 +0800

    Initial Commit

---
+++ ToolExecutor/ToolExecutorApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToolExecutorApp.swift
+//  ToolExecutor
+//
+//  Created by p304 on 2025/7/18.
+//
+
+import SwiftUI
+
+@main
+struct ToolExecutorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ToolExecutorTests/ToolExecutorTests.swift
@@ -0,0 +1,16 @@
+//
+//  ToolExecutorTests.swift
+//  ToolExecutorTests
+//
+//  Created by p304 on 2025/7/18.
+//
+
+import Testing
+
+struct ToolExecutorTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
